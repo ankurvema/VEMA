4de6214ab5fd7e795b5904f8b27dc1ea3eb209b3
ankurvema <4ankurc@gmail.com>
2024-04-24 21:05:49 -0700

    Updated basic project files

---
+++ VEMA/VEMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  VEMAApp.swift
+//  VEMA
+//
+//  Created by Ankur Chauhan on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct VEMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
